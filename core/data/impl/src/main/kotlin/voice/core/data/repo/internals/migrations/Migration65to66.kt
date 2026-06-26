8c08d213fcc6f3d1ca7951bdfe06bfea6c429645
Brad Pitcher <bradpitcher@gmail.com>
2026-06-26 18:58:58 +0000

    linting

--- core/data/impl/src/main/kotlin/voice/core/data/repo/internals/migrations/Migration65to66.kt
+++ core/data/impl/src/main/kotlin/voice/core/data/repo/internals/migrations/Migration65to66.kt
@@ -1,10 +1,10 @@
 package voice.core.data.repo.internals.migrations
 
-import androidx.sqlite.db.SupportSQLiteDatabase
 import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.binding
 import dev.zacsweers.metro.ContributesIntoSet
+import dev.zacsweers.metro.binding
 
 @ContributesIntoSet(
   scope = AppScope::class,
